970793c3484d90a2fc74dc771fc66f57c5361c86
ikawaha <ikawaha@users.noreply.github.com>
2019-12-02 21:24:42 +0900

    Add comments

--- checker.go
+++ checker.go
@@ -17,10 +17,13 @@ type (
 	errorHandler = func(context.Context, http.ResponseWriter, error)
 	middleware   = func(http.Handler) http.Handler
 
+	// HandlerBuilder represents the goa http handler builder.
 	HandlerBuilder func(goa.Endpoint, goahttp.Muxer, decoder, encoder, errorHandler) http.Handler
+	// HandlerMounter represents the goa http handler mounter.
 	HandlerMounter func(goahttp.Muxer, http.Handler)
 )
 
+// APIChecker represents the API checker.
 type APIChecker struct {
 	Mux          goahttp.Muxer
 	Middleware   []middleware
@@ -29,32 +32,38 @@ type APIChecker struct {
 	ErrorHandler errorHandler
 }
 
+// Option represents options for API checker.
 type Option func(c *APIChecker)
 
+// Muxer sets the goa http multiplexer.
 func Muxer(mux goahttp.Muxer) Option {
 	return func(c *APIChecker) {
 		c.Mux = mux
 	}
 }
 
+// Decoder sets the decoder.
 func Decoder(dec decoder) Option {
 	return func(c *APIChecker) {
 		c.Decoder = dec
 	}
 }
 
+// Encoder sets the encoder.
 func Encoder(enc encoder) Option {
 	return func(c *APIChecker) {
 		c.Encoder = enc
 	}
 }
 
+// ErrorHandler sets the error handler.
 func ErrorHandler(eh errorHandler) Option {
 	return func(c *APIChecker) {
 		c.ErrorHandler = eh
 	}
 }
 
+// New constructs API checker.
 func New(options ...Option) *APIChecker {
 	ret := &APIChecker{
 		Mux:        goahttp.NewMuxer(),
@@ -71,14 +80,19 @@ func New(options ...Option) *APIChecker {
 	return ret
 }
 
+// Mount mounts the endpoint handler.
 func (c *APIChecker) Mount(builder HandlerBuilder, mounter HandlerMounter, endpoint goa.Endpoint) {
 	handler := builder(endpoint, c.Mux, c.Decoder, c.Encoder, c.ErrorHandler)
 	mounter(c.Mux, handler)
 }
+
+// Use sets the middleware.
 func (c *APIChecker) Use(middleware func(http.Handler) http.Handler) {
 	c.Middleware = append(c.Middleware, middleware)
 }
 
+// Test returns a http checker that tests the endpoint.
+// see. https://github.com/ivpusic/httpcheck/
 func (c APIChecker) Test(t *testing.T, method, path string) *httpcheck.Checker {
 	var handler http.Handler = c.Mux
 	for _, v := range c.Middleware {
